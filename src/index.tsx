9aad28f1f1ef0bd36af41f3d16a901d73eebf106
Hyesung Oh <haesungoh414@gmail.com>
2021-08-31 17:13:34 +0900

    #5 set RecoilRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import ReactDOM from "react-dom";
 import App from "components/App";
 
+// for styled-components
 import { ThemeProvider } from "styled-components";
 import theme from "styles/theme";
 import GlobalStyle from "styles/global";
 
+// for recoil
+import { RecoilRoot } from "recoil";
+
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-    </ThemeProvider>,
+    <RecoilRoot>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <App />
+        </ThemeProvider>
+    </RecoilRoot>,
     document.getElementById("root")
 );
