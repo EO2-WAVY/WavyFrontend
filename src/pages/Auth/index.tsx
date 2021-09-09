4aa2b111bdd5a218ac67ded2449767de22a95469
Hyesung Oh <haesungoh414@gmail.com>
2021-09-10 00:41:46 +0900

    [#12] apply onClickLogo event

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+
+import Footer from "components/Common/Footer";
+import Nav from "components/Common/Nav";
+
 const Auth = () => {
-    return <div>Auth</div>;
+    return (
+        <>
+            <Nav />
+            <Main />
+            <Footer />
+        </>
+    );
 };
 
 export default Auth;
+
+const Main = styled.main`
+    height: 100vh;
+`;
