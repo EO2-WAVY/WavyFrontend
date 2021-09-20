d24ff861da77c5bd438f956e61e34e453db5163d
Hyesung Oh <haesungoh414@gmail.com>
2021-09-21 01:16:46 +0900

    [#17] apply fetcher and axios instance. method

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -3,3 +3,15 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "localhost:8000",
 });
+
+export const get = <T>(...args: Parameters<typeof instance.get>) =>
+    instance.get<T, T>(...args);
+
+export const post = <T>(...args: Parameters<typeof instance.post>) =>
+    instance.post<T, T>(...args);
+
+export const put = <T>(...args: Parameters<typeof instance.put>) =>
+    instance.put<T, T>(...args);
+
+export const del = <T>(...args: Parameters<typeof instance.delete>) =>
+    instance.delete<T, T>(...args);
--- src/utils/api/fetch.ts
+++ src/utils/api/fetch.ts
@@ -1,1 +1,7 @@
-export const fetcher = () => {};
+import { get } from "utils/api/client";
+
+export const fetcher = async <T>(path: string) => {
+    const response = await get<T>(path);
+
+    return response;
+};
