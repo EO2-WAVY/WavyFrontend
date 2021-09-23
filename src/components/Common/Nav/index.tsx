e457ecaf7651fdaacfa08bfae60f10655483d23a
Hyesung Oh <haesungoh414@gmail.com>
2021-09-24 00:44:16 +0900

    [#28] apply conditional nav with mouse position when dance

--- src/components/Common/Nav/index.tsx
+++ src/components/Common/Nav/index.tsx
@@ -6,22 +6,20 @@ import NavSearchBar from "components/Common/Nav/NavSearchBar";
 import NavAuthBtn from "components/Common/Nav/NavAuthBtn";
 import { navVariants } from "constants/motions";
 import useMousePosition from "hooks/useMousePosition";
-
-
+import useNavCondition from "hooks/useNavCondition";
 
 const Nav = () => {
     const history = useHistory();
     const onClickLogo = () => history.push("/");
 
     const { mousePosition } = useMousePosition();
+    const { isConditional } = useNavCondition();
 
     return (
         <AnimatePresence exitBeforeEnter>
             <Navbar
-                variants={navVariants}
-                initial="initial"
-                animate="animate"
-                exit="exit"
+                isConditional={isConditional}
+                isHover={mousePosition.y < 104}
             >
                 <Layout>
                     <LeftElem>
@@ -46,15 +44,28 @@ const Nav = () => {
 
 export default Nav;
 
-const Navbar = styled(motion.nav)`
-    position: -webkit-sticky;
-    position: sticky;
+interface NavbarProps {
+    isConditional: boolean;
+    isHover: boolean;
+}
+
+const Navbar = styled(motion.nav)<NavbarProps>`
+    position: ${({ isConditional }) => (isConditional ? "absolute" : "sticky")};
     top: 0px;
     width: 100vw;
     height: 104px;
     background-color: ${({ theme }) => theme.color.white};
     box-shadow: 0px 10px 25px 4px rgba(0, 0, 0, 0.03);
     z-index: 10;
+
+    transition: transform 0.8s cubic-bezier(0.6, -0.05, 0.01, 0.99);
+    transform: translateY(
+        ${({ isHover, isConditional }) => {
+            if (!isConditional) return "0";
+            else if (isConditional && isHover) return "0";
+            return "-104px";
+        }}
+    );
 `;
 
 const Layout = styled.div`
