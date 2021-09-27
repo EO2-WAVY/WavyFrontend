f8e40ebfa120ef6bae93392008829fd8622713be
Hyesung Oh <haesungoh414@gmail.com>
2021-09-27 20:05:01 +0900

    [#32] apply index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,17 @@
+// images
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
+
+// fonts
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.ttf";
+declare module "*.eot";
+declare module "*.otf";
+
+// videos
+declare module "*.mp4";
+declare module "*.webm";
