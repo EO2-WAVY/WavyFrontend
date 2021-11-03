096d53477330791f55aed77da8bba9c1cc86cdf0
Hyesung Oh <haesungoh414@gmail.com>
2021-11-04 02:01:13 +0900

    [#91] apply basic logic of analysis pictogram

--- src/components/Analysis/AnalysisPictogram.tsx
+++ src/components/Analysis/AnalysisPictogram.tsx
@@ -4,6 +4,8 @@ import Icon from "components/Common/Icon";
 import useControllerPlayedSecond from "hooks/Dance/Controller/useControllerPlayedSecond";
 import useGetAnalysis from "hooks/api/useGetAnalysis";
 import { defaultFadeInDownVariaints } from "constants/motions";
+import { useEffect, useState } from "react";
+import { min } from "lodash";
 
 interface AnalysisPictogramProps {
     anSeq: string;
@@ -11,11 +13,45 @@ interface AnalysisPictogramProps {
 
 const AnalysisPictogram = ({ anSeq }: AnalysisPictogramProps) => {
     const { playedSecond } = useControllerPlayedSecond();
+    const flooredPlayedSecond = Math.floor(playedSecond);
     const { data, isAnalysed } = useGetAnalysis(anSeq);
-    
-    console.log(playedSecond);
 
-    console.log(data?.simularityJson.analyzes[0].scores);
+    const [isLHandWrong, setLHandWrong] = useState<boolean>(false);
+    const [isRHandWrong, setRHandWrong] = useState<boolean>(false);
+    const [isLLegWrong, setLLegWrong] = useState<boolean>(false);
+    const [isRLegWrong, setRLegWrong] = useState<boolean>(false);
+
+    useEffect(() => {
+        const isWrong = (
+            index1: number,
+            index2: number,
+            index3: number
+        ): boolean => {
+            if (!data) return false;
+            const { analyzes } = data.simularityJson;
+            const { scores } = analyzes[flooredPlayedSecond]
+                ? analyzes[flooredPlayedSecond]
+                : { scores: [0, 0, 0] };
+
+            const minScore = min([
+                scores[index1],
+                scores[index2],
+                scores[index3],
+            ]);
+
+            if (!minScore || minScore < 75) {
+                return true;
+            }
+            return false;
+        };
+        console.log(flooredPlayedSecond);
+
+        setLHandWrong(isWrong(5, 7, 9));
+        setRHandWrong(isWrong(6, 8, 10));
+        setLLegWrong(isWrong(11, 13, 15));
+        setRLegWrong(isWrong(12, 14, 16));
+    }, [data, flooredPlayedSecond]);
+
     return (
         <AnimatePresence exitBeforeEnter>
             {isAnalysed && (
@@ -26,10 +62,10 @@ const AnalysisPictogram = ({ anSeq }: AnalysisPictogramProps) => {
                     exit="exit"
                 >
                     <Icon name="analysis_pictogram" />
-                    <Icon name="analysis_lHandWrong" />
-                    <Icon name="analysis_rHandWrong" />
-                    <Icon name="analysis_lLegWrong" />
-                    <Icon name="analysis_rLegWrong" />
+                    {isLHandWrong && <Icon name="analysis_lHandWrong" />}
+                    {isRHandWrong && <Icon name="analysis_rHandWrong" />}
+                    {isLLegWrong && <Icon name="analysis_lLegWrong" />}
+                    {isRLegWrong && <Icon name="analysis_rLegWrong" />}
                 </Wrapper>
             )}
         </AnimatePresence>
